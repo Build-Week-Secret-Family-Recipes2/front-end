87dd1b882df1330026245d640ee4e3e08668ffed
vospader13 <justenmcdonald@gmail.com>
2020-05-21 18:18:12 -0600

    updated recipe card template

--- family-recipes/src/Components/RecipeCard.js
+++ family-recipes/src/Components/RecipeCard.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -13,6 +15,61 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 
+
+
+const CardWrapper = styled.div`
+    display: flex;
+    margin: auto;
+    padding: 0;
+    justify-content: evenly;
+    flex-flow: row wrap;
+   
+`;
+
+const Header = styled.h1`
+color: #FD8664;
+font-size: 3.6rem;
+text-align: center;
+`
+
+function RecipesPage() {
+
+    const [recipes, SetRecipes] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://backend-chef.herokuapp.com/api/recipes/`)
+            .then(res => {
+                SetRecipes(res.data);
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log('this data was not returned', err);
+            })
+    }, []);
+
+    return (
+      
+        <section >
+            <Header>Recipes</Header>      
+            {/* add search bar here */}
+            <CardWrapper className="Recipe-Day" >                
+                    {recipes.map(recipes => (
+                        <RecipeCard 
+                        chef_name={recipes.chef_name}
+                        recipe_photo={recipes.recipe_photo}
+                        recipe_name={recipes.recipe_name} 
+                        ingredients={recipes.ingredients}
+                        cook_time={recipes.cook_time}
+                        prep_time={recipes.prep_time}
+                        instructions={recipes.instructions}
+                        servings={recipes.servings}
+                        />
+                    ))}                
+            </CardWrapper>                  
+        </section>
+    );
+}
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -56,13 +113,16 @@ export default function RecipeCard( {recipe_name, recipe_photo, ingredients, coo
             <MoreVertIcon />
           </IconButton>
         }
-        title= {recipe_name}
+
         
       />
       <CardMedia
         className={classes.media}
         image={recipe_photo}
       />
+
+        title= {recipe_name}
+
       <CardActions disableSpacing>
         
         <IconButton
