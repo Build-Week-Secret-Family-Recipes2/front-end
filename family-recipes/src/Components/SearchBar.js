37651931b7107a011a400721f72e74e9ca7bea3f
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-05-21 21:56:46 -0400

    Setting up what functions and states will look like once we've got the api

--- family-recipes/src/Components/SearchBar.js
+++ family-recipes/src/Components/SearchBar.js
@@ -1,6 +1,19 @@
 import React from "react";
+// ^will need to import useState and useHook
+
 
 function SearchBar() {
+    // const [recipes, setRecipes] = useState("");
+    // ^axios call to pull in recipes data and set that data to setRecipes while using useHook
+
+    // const [word, setWord] = useState("");
+    // ^this will track any change to the input in the filter box
+
+    // const [filterDisplay, setFilterDisplay] = useState("recipes")
+
+    // const handleChange = e => {let oldList = recipes.map(meal => { return {recipe: meal.recipes}})}
+
+
     return (
         <div>
 
