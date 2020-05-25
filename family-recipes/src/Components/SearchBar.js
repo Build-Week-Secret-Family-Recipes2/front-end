91c798d5c7b45b806b5a51f07430f5d690f41c07
Amy Davis <amydavis@Blakes-MacBook-Pro-2.local>
2020-05-24 20:14:32 -0400

    Adding state and onChange for search bar

--- family-recipes/src/Components/SearchBar.js
+++ family-recipes/src/Components/SearchBar.js
@@ -10,13 +10,28 @@ function SearchBar() {
     // ^this will track any change to the input in the filter box
 
     // const [filterDisplay, setFilterDisplay] = useState("recipes")
+    // ^this will display the list depending on what's typed in the search bar
 
-    // const handleChange = e => {let oldList = recipes.map(meal => { return {recipe: meal.recipes}})}
+    // const handleChange = e => {let oldList = recipes.map(meal => { return {recipe: meal.recipes.toLowerCase()};}); 
+
+    // if (e !== "") { let newList = [];
+    // setWord(e); newList = oldList.filter(meal => meal.recipes.includes(word.toLowerCase()));
+
+    // setFilterDisplay(newList);
+    // } else { setFilterDisplay(recipes);}};}
 
 
     return (
         <div>
-
+            <h2>Search Your Recipes</h2>
+            {/* <input onChange={e => handleChange(e.target.value)} />
+            {filterDisplay.map(recipes, i) => (
+                <div key={i}>
+                    <li>
+                        {recipes.recipe} &nbsp;
+                    </li>
+                </div>
+            )} */}
         </div>
     )
 }
